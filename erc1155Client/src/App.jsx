ca45349f3d5d5f885551b1f4ba6b56f0223bb6a1
Adidem23 <adityasuryawanshi5451@gmail.com>
2023-09-06 23:30:00 +0530

    Second Commit

--- erc1155Client/src/App.jsx
+++ erc1155Client/src/App.jsx
@@ -2,7 +2,7 @@ import ComposeAll from "./Components/ComposeAll"
 import { Routes, Route } from 'react-router-dom'
 import MintNFT from "./Components/MintNFT"
 import { ThirdwebSDKProvider } from '@thirdweb-dev/react'
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
 function App() {
 
@@ -10,10 +10,11 @@ function App() {
 
   return (
     <>
-      <ThirdwebSDKProvider activeChain={ActiveChain} signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()} clientId="5fb26c268ed64fb73d9fb6010411dca9">
+      <ThirdwebSDKProvider activeChain={ActiveChain} signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
+        clientId="5fb26c268ed64fb73d9fb6010411dca9">
         <Routes>
           <Route path="/" Component={ComposeAll} />
-          <Route path="/MintNft" Component={MintNFT} />
+          <Route path="/mintnft" Component={MintNFT} />
         </Routes>
       </ThirdwebSDKProvider>
     </>
--- erc1155Client/src/Components/MintNFT.jsx
+++ erc1155Client/src/Components/MintNFT.jsx
@@ -1,15 +1,17 @@
 import '../CSS/MintNFT.css'
 import { useState } from 'react'
-import { ThirdwebSDK } from '@thirdweb-dev/react';
-import {  ethers } from 'ethers';
+import { ThirdwebSDK, useContract } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
 
 const MintNFT = () => {
 
     const [NFTName, setNFTName] = useState("Name");
     const [NFTDescription, setNFTDescription] = useState("Description")
     const [SupplyNumber, setSupplyNumber] = useState(0)
-    const [Image, setImage] = useState("")
+    const [Image, setImage] = useState(null)
+    const [ClickedForImage, setClickedForImage] = useState(false)
     const [Account, setAccount] = useState("")
+    const { contract } = useContract("0xDC5A319945E0c84e7A559DC2FbCC99fB7f88c43a")
 
     const InputNameHandle = async (e) => {
         setNFTName(e.target.value);
@@ -23,8 +25,24 @@ const MintNFT = () => {
         setSupplyNumber(e.target.value);
     }
 
-    const HandleFile = async (e) => {
-        setImage(e.target.value);
+    const HandleFile = async (event) => {
+
+        setClickedForImage(true);
+
+        const selectedImage = event.target.files[0];
+
+        if (selectedImage) {
+            const reader = new FileReader();
+
+            reader.onload = (e) => {
+                const fileContent = e.target.result;
+                console.log(fileContent);
+            };
+
+            setImage(URL.createObjectURL(selectedImage))
+        }
+
+
     }
 
     const ConnectToMetamask = async () => {
@@ -43,6 +61,7 @@ const MintNFT = () => {
     }
     ConnectToMetamask();
 
+
     const MintNftGiven = async (e) => {
         e.preventDefault();
         try {
@@ -55,26 +74,27 @@ const MintNFT = () => {
 
     const ListNFT = async () => {
 
-        const Metadata = {
-            name: "Aditya",
-            description: "Aditya Suryawanshi",
-            image: 'https://media.npr.org/assets/img/2014/01/02/140102123909_1_wide-da99ab742e0a327bf840d4b8e01b864b019482ca-s1100-c50.jpg'
-            ,
-        }
-
         const providers = new ethers.providers.Web3Provider(window.ethereum);
+        const Signers = new ethers.Wallet("5ad7f7823ac4a9518b1ce47b007c63c150bc31382d6878d48cce4abb2cc707ef", providers);
 
-        const Signer = new ethers.Wallet("5ad7f7823ac4a9518b1ce47b007c63c150bc31382d6878d48cce4abb2cc707ef", providers);
+        const sdk = ThirdwebSDK.fromSigner(Signers)
+        sdk.wallet.connect(Signers);
+        const imageuploaded=document.getElementById('imageuploaded').innerHTML;
 
-        const sdkt = ThirdwebSDK.fromSigner(Signer);
+        const ContractAsa = await sdk.getContract("0xDC5A319945E0c84e7A559DC2FbCC99fB7f88c43a");
+        await ContractAsa.roles.grant("minter", Account);
 
-        sdkt.wallet.connect(Signer);
+        const MetadataWithSupply = {
+            supply: Number(SupplyNumber),
+            metadata: {
+                name: NFTName,
+                description: NFTDescription,
+                image: imageuploaded
+            }
+        }
+        const signedPayload = await contract.erc1155.mintTo(Account, MetadataWithSupply);
+        console.log(signedPayload);
 
-        const contractnew = await sdkt.getContract("0xDC5A319945E0c84e7A559DC2FbCC99fB7f88c43a");
-
-        await contractnew.roles.grant("minter", Account);
-
-        const signedPayload = await contractnew.erc1155.mintTo(Account, Metadata);
     }
 
     return (
@@ -95,11 +115,11 @@ const MintNFT = () => {
                                         <div className="title">Enter Metedata</div>
                                         <form className="flip-card__form" action="">
 
-                                            <input className="flip-card__input" placeholder="Name" type="text" onChange={InputNameHandle} value={NFTName} />
+                                            <input className="flip-card__input" type="text" onChange={InputNameHandle} />
 
-                                            <input className="flip-card__input" placeholder="Description" type="text" onChange={InputDescHandle} value={NFTDescription} />
+                                            <input className="flip-card__input" type="text" onChange={InputDescHandle} />
 
-                                            <input className="flip-card__input" placeholder="Supply" type="number" onChange={InputSupplyHandle} value={SupplyNumber} />
+                                            <input className="flip-card__input" type="number" onChange={InputSupplyHandle} />
 
                                         </form>
                                         <div>
@@ -108,16 +128,15 @@ const MintNFT = () => {
                                     </div>
 
                                     <div className="flip-card__back">
-                                        <label className="custum-file-upload" htmlFor="file">
+                                        {!ClickedForImage ? <label className="custum-file-upload" htmlFor="file">
                                             <div className="icon">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24"><g id="SVGRepo_iconCarrier"> <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" ></path> </g></svg>
                                             </div>
-                                            <div className="text">
+                                            <div className="text" id='ImageUpload'>
                                                 <span>Click to upload image</span>
                                             </div>
-                                            <input type="file" id="file" onChange={HandleFile} value={Image} />
-                                        </label>
-
+                                            <input type="file" accept='image/*' id="file" onChange={HandleFile} value={Image} />
+                                        </label> : <img src={Image} style={{ background: "#000" }} id='imageuploaded' />}
                                     </div>
 
 
